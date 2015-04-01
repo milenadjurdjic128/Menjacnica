74a201f7c0943146745f20356369926412d33177
milenadjurdjic128 <milena.djurdja@gmail.com>
2015-04-01 19:10:05 +0200

    Napravljena klasa Menjacnica koja implementira metode interfejsa

---
+++ Menjacnica/src/interfejs/Menjacnica.java
@@ -0,0 +1,24 @@
+package interfejs;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.Valuta;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(Valuta nova) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean izbrisiKurs(Valuta nepotrebna) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public double pronadjiKurs(GregorianCalendar dat, String trazena) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
